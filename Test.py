fda42d8af6786980cb8e329ede466781689264ad
henrany <tmkhenry@gmail.com>
2022-01-08 19:54:21 -0300

    unit testing

--- Test.py
+++ Test.py
@@ -6,7 +6,6 @@ class TestAlgorithms(unittest.TestCase):
     def setUp(self):
         self.l = [9,4,6,2,1,3,5,7,8]
         self.expected = [1,2,3,4,5,6,7,8,9]
-        self.bubble = bubbleSort()
 
     def testBubbleSort(self):
-        self.assertEqual(self.bubble(self.l), self.expected)
+        self.assertEqual(bubbleSort(self.l), self.expected)
